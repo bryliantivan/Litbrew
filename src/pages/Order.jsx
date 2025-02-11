682e43bcb854b572fa9bdead936353b05fb858ef
paundrarz <paundrarangga31@gmail.com>
2025-02-11 09:08:57 +0700

    update pada order tracking dan order

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -211,12 +211,12 @@ const Order = () => {
                       </dl>
                       <dl className="flex items-center justify-between gap-4">
                         <dt className="text-base font-normal text-gray-500 dark:text-gray-400">Tax (11%)</dt>
-                        <dd className="text-base font-medium text-gray-900 dark:text-white">IDR {tax.toFixed(2).toLocaleString('id-ID')}</dd>
+                        <dd className="text-base font-medium text-gray-900 dark:text-white">IDR {tax.toLocaleString('id-ID')}</dd>
                       </dl>
                     </div>
                     <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
                       <dt className="text-base font-bold text-gray-900 dark:text-white">Total</dt>
-                      <dd className="text-base font-bold text-gray-900 dark:text-white">IDR {totalWithTax.toFixed(2).toLocaleString('id-ID')}</dd>
+                      <dd className="text-base font-bold text-gray-900 dark:text-white">IDR {totalWithTax.toLocaleString('id-ID')}</dd>
                     </dl>
                   </div>
                   <button onClick={handleCheckout} className="flex w-full items-center justify-center rounded-lg bg-[#AAE8ED] px-5 py-2.5 text-sm font-medium text-black hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Proceed to Checkout</button>
