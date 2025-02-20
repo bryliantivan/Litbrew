150feeb3463c8b84f8ce26c20ee574c9751567e3
fransiskafu <fransiskafu2601@gmail.com>
2025-02-20 23:21:28 +0700

    Penyelesaian FrontEnd AdminManageMenu.jsx

--- src/pages/AdminManageMenu.jsx
+++ src/pages/AdminManageMenu.jsx
@@ -3,64 +3,82 @@ import { useState, useEffect } from 'react';
 const AdminManageMenu = () => {
     const menuItems = [
         //... your menu item data
+        {
+            id: 1,
+            name: "Peach Tea",
+            image:
+                "https://res.cloudinary.com/dhwvjtkyw/image/upload/v1734870610/glgfid2qpwcerevrpedw.png",
+            description:
+                "A refreshing and sweet iced tea infused with the juicy flavor of ripe peaches. Perfect for a light and fruity pick-me-up.",
+            price: 10000,
+            countInStock: 14,
+            rating: 4.8, // Ganti dengan nilai rating yang valid
+            numReview: 6, // Ganti dengan nilai review count yang valid
+            category: "Drink"
+        },
+        //... more menu items
     ];
 
     return (
-        <div className="container mx-auto p-7">
-            <h1 className="text-2xl font-bold mb-4">MENUS</h1>
+        <div className="container mx-auto mt-[8vw] p-7">
+            <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                    <h1 className="text-2xl font-bold font-raleway">MENUS</h1>
+                    <button className="bg-[#334147] hover:bg-[#07779D] text-white font-raleway font-medium py-2 px-4 rounded-md ml-4">
+                        + Add New
+                    </button>
+                </div>
 
-            <div className="flex items-center mb-4">
-                <input
-                    type="text"
-                    placeholder="Search Menu"
-                    className="border border-gray-400 px-3 py-2 rounded-md w-full mr-2"
-                />
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md">
-                    <svg
-                        className="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                        />
-                    </svg>
-                </button>
+                <div className="relative ml-4 items-center">
+                    <input
+                        type="text"
+                        placeholder="Search Menu"
+                        className="border border-[#07779D] px-[1vw] py-[1vw] rounded-[0.5vw] focus:outline-none w-[30vw]"
+                    />
+                    <button className="absolute right-[0.5vw] top-[0.5vw] bg-[#07779D] text-white font-medium font-raleway
+                                            py-2 px-2 rounded-full ">
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            />
+                        </svg>
+                    </button>
+                </div>
             </div>
 
-            <button className="bg-green-500 hover:bg-green-700 text-white font-medium py-2 px-4 rounded-md mb-4">
-                + Add New
-            </button>
-
-            <table className="table-fixed w-full border-collapse">
+            <table className="table-fixed w-full border-collapse mt-[2vw]">
                 <thead>
-                    <tr className="bg-gray-100">
-                        <th className="p-4 border font-medium text-gray-500">ID</th>
-                        <th className="p-4 border font-medium text-gray-500">NAME</th>
-                        <th className="p-4 border font-medium text-gray-500">DESCRIPTION</th>
-                        <th className="p-4 border font-medium text-gray-500">PRICE</th>
-                        <th className="p-4 border font-medium text-gray-500">STOCK</th>
-                        <th className="p-4 border font-medium text-gray-500">RATING</th>
-                        <th className="p-4 border font-medium text-gray-500">REVIEW COUNT</th>
-                        <th className="p-4 border font-medium text-gray-500">CATEGORY</th>
-                        <th className="p-4 border font-medium text-gray-500">MODIFY</th>
+                    <tr className="bg-[#AAE8ED]">
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[3vw]">ID</th>
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[10vw]">NAME</th>
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[20vw]">DESCRIPTION</th>
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[7vw]">PRICE</th>
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[5vw]">STOCK</th>
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[5vw]">RATING</th>
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[10vw]">REVIEW COUNT</th>
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[7vw]">CATEGORY</th>
+                        <th className="px-[0.5vw] py-[1vw] border font-bold font-raleway text-black w-[7vw]">MODIFY</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {menuItems.map((item) => (
-                        <tr key={item.id} className="hover:bg-gray-200">
-                            <td className="p-4 border text-center">{item.id}</td>
+                    {menuItems.map((item, index) => (
+                        <tr key={index} className="hover:bg-gray-200">
+                            <td className="p-4 border text-center">{index + 1}</td> 
                             <td className="p-4 border">{item.name}</td>
                             <td className="p-4 border">{item.description}</td>
                             <td className="p-4 border text-center">{item.price}</td>
-                            <td className="p-4 border text-center">{item.stock}</td>
+                            <td className="p-4 border text-center">{item.countInStock}</td>
                             <td className="p-4 border text-center">{item.rating}</td>
-                            <td className="p-4 border text-center">{item.reviewCount}</td>
+                            <td className="p-4 border text-center">{item.numReview}</td>
                             <td className="p-4 border text-center">{item.category}</td>
                             <td className="p-4 border text-center">
                                 <button className="text-blue-500 hover:text-blue-700 mr-2">
