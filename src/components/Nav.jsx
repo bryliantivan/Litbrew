50dc2b88ed3d3ccf6ad58b2aee966d3cd00cc99f
Ivan Bryliant <bryliantivan@gmail.com>
2025-02-25 15:53:00 +0700

    Menambahkan hamburger navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,6 +13,8 @@ const Nav = () => {
     const [cart, setCart] = useState(JSON.parse(localStorage.getItem('cart')) || []);
     const [loadingUser, setLoadingUser] = useState(false); // State untuk loading indicator
     const [userError, setUserError] = useState(null); // State untuk error message
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State for hamburger menu
+    const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
     // Function to check the token in localStorage
     const checkToken = () => {
@@ -101,7 +103,8 @@ const Nav = () => {
                 <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="/logoLitbrew.svg" className="h-14" alt="Litbrew Logo" />
                 </NavLink>
-                <div className="flex md:order-2 space-x-4 rtl:space-x-reverse">
+
+                <div className="flex max-md:ml-auto md:order-2 space-x-4 rtl:space-x-reverse">
                     {!isLoggedIn ? (
                         <Link to="/login">
                             <button
@@ -124,7 +127,7 @@ const Nav = () => {
                                                 </p>
                                             </div>
                                         )}
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="mt-4 h-8 w-8">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="mt-4 h-8 w-8 text-blue-200">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                         </svg>
                                     </div>
@@ -144,59 +147,75 @@ const Nav = () => {
                                 </button>
                                 {dropdownOpen && user && (
                                     <div className="absolute right-0 mt-2 w-64 bg-white rounded-lg shadow-lg py-4 px-6 z-50 font-raleway font-bold border border-gray-200">
-                                         {loadingUser ? (
+                                        {loadingUser ? (
                                             <p>Loading user data...</p>
                                         ) : userError ? (
                                             <p className="text-red-500">{userError}</p>
                                         ) : (
-                                        <>
-                                        <div className="flex items-center space-x-3 mb-3">
-                                            <div className="w-8 h-8 rounded-full bg-gray-300">
-                                                {/* Placeholder for user avatar */}
-                                            </div>
-                                            <div>
-                                                <p className="text-sm text-gray-800 font-semibold">{user.name}</p>
-                                                <p className="text-xs text-gray-500">{user.email}</p>
-                                            </div>
-                                        </div>
-                                        <div className="flex gap-[0.5vw] text-l text-gray-800 mb-3">
-                                            <img src={xp} alt="XP Icon" />
-                                            <p>XP Points: {user.points}</p>
-                                        </div>
-                                        <div>
-                                            <Link
-                                                to="/order"
-                                                className="block text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-300 rounded"
-                                            >
-                                                My Order
-                                            </Link>
-                                        </div>
+                                            <>
+                                                <div className="flex items-center space-x-3 mb-3">
+                                                    <div className="w-8 h-8 rounded-full bg-gray-300">
+                                                        {/* Placeholder for user avatar */}
+                                                    </div>
+                                                    <div>
+                                                        <p className="text-sm text-gray-800 font-semibold">{user.name}</p>
+                                                        <p className="text-xs text-gray-500">{user.email}</p>
+                                                    </div>
+                                                </div>
+                                                <div className="flex gap-[0.5vw] text-l text-gray-800 mb-3">
+                                                    <img src={xp} alt="XP Icon" />
+                                                    <p>XP Points: {user.points}</p>
+                                                </div>
+                                                <div>
+                                                    <Link
+                                                        to="/order"
+                                                        className="block text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-300 rounded"
+                                                    >
+                                                        My Order
+                                                    </Link>
+                                                </div>
 
-                                        <div>
-                                            <Link
-                                                to="/profile"
-                                                className="block text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-300 rounded"
-                                            >
-                                                Profile Settings
-                                            </Link>
-                                        </div>
+                                                <div>
+                                                    <Link
+                                                        to="/profile"
+                                                        className="block text-left px-4 py-2 text-sm text-gray-700 hover:bg-blue-300 rounded"
+                                                    >
+                                                        Profile Settings
+                                                    </Link>
+                                                </div>
 
-                                        <button
-                                            onClick={handleLogout}
-                                            className="block text-left w-full px-4 py-2 text-sm text-gray-700 hover:bg-blue-300 rounded"
-                                        >
-                                            Sign Out
-                                        </button>
-                                    </>
-                                     )}
+                                                <button
+                                                    onClick={handleLogout}
+                                                    className="block text-left w-full px-4 py-2 text-sm text-gray-700 hover:bg-blue-300 rounded"
+                                                >
+                                                    Sign Out
+                                                </button>
+                                            </>
+                                        )}
                                     </div>
                                 )}
                             </div>
                         </>
                     )}
                 </div>
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#07779D]">
+                
+                {/* Hamburger button for mobile view */}
+                <button
+                    onClick={toggleMobileMenu}
+                    type="button"
+                    className="inline-flex items-center mr-[3vw] p-2 w-10 h-10 justify-center mt-2 text-sm text-blue-200 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 text-xl focus:ring-gray-200 dark:text-gray-400"
+                    aria-controls="navbar-sticky"
+                    aria-expanded={isMobileMenuOpen}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+
+                {/* Navigation links, conditionally rendered based on mobile menu state */}
+                <div className={`${isMobileMenuOpen ? '' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`} id="navbar-sticky">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-800 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-[#07779D]">
                         {navLinks.map((link) => (
                             <li key={link.label}>
                                 <NavLink
