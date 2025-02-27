4315b0cec20b61c3320f55621c3950815525bb2c
skzvy <steffieadriana999@gmail.com>
2025-02-27 15:55:27 +0700

    Menambahkan revisi admin pages

--- src/pages/AdminManageOrders.jsx
+++ src/pages/AdminManageOrders.jsx
@@ -109,7 +109,7 @@ const AdminManageOrders = () => {
             value={searchTerm}
             onChange={handleSearchChange}
         />
-        <button className="absolute right-2 top-2 bg-[#07779D] text-white font-medium font-raleway p-2 rounded-full">
+        <button className="absolute mt-[0.8%] right-2 bg-[#07779D] text-white font-medium font-raleway p-2 rounded-full">
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
